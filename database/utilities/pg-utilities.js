47684e23b56674ffa437f60169ea5af4da89a090
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-04-21 23:34:41 -0700

    (cleanup) db utilities

--- database/utilities/pg-utilities.js
+++ database/utilities/pg-utilities.js
@@ -1,9 +1,9 @@
-const pg_constants = require('../pg-constants');
+const pgConstants = require('../pg-constants');
 
 // Takes a number and returns a string of pg params up to that numer
 const createParamsStr = limit => {
   let paramsStr = '';
-  
+
   for (let i = 1; i < limit + 1; i++) {
     if (i === limit) {
       paramsStr = `${paramsStr} $${i}`;
@@ -11,14 +11,14 @@ const createParamsStr = limit => {
       paramsStr = `${paramsStr} $${i}, `;
     }
   }
-  
+
   return paramsStr;
 };
 
 // Takes in a count of params and creates the 'VALUES' parameters string
 const createValuesParams = count => {
-  const { VALUES } = pg_constants;
-  
+  const { VALUES } = pgConstants;
+
   return `${VALUES} (${createParamsStr(count)})`;
 };
 
