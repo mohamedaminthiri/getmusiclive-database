c46b4bf3e76be36f48443b892136068305cee03b
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-07-29 01:23:14 -0700

    [Eventbrite] (feat): making paginated requests

--- src/data/eventbrite/utilities/request.js
+++ src/data/eventbrite/utilities/request.js
@@ -1,5 +1,6 @@
 /* eslint no-console: 0 */
 const { get: axiosGet } = require('axios');
+const { range } = require('lodash');
 const { getLocations } = require('./get-locations');
 const { eventsFormat } = require('./format-events');
 
@@ -17,14 +18,30 @@ const EVENTBRITE_KEY = `&token=${process.env.EVENTBRITE_KEY}`;
 const SEARCH_URL = '?sort_by=date&location.address=1451+7th+St%2C+Oakland%2C+CA+94607&location.within=50mi&categories=103&start_date.keyword=this_month&expand=venue';
 const BASE_URL = 'https://www.eventbriteapi.com/v3/events/search/';
 const EVENTBRITE_URL = `${BASE_URL}${SEARCH_URL}`;
+const PAGE = 'page';
 const eventbrite = `${EVENTBRITE_URL}${EVENTBRITE_KEY}`;
-console.log(eventbrite);
+
+const addQuery = (key, value) => {
+  const pair = `&${key}=${value}`;
+
+  return `${EVENTBRITE_URL}${pair}${EVENTBRITE_KEY}`;
+};
+
 const eventbriteReq = axiosGet(eventbrite);
+const paginationRequest = ({ page_count: pageCount }) => {
+  const reqs = range(2, pageCount + 1);
+
+  return reqs.map(count => axiosGet(addQuery(PAGE, count)));
+};
 
 eventbriteReq
-  .then(({ data: { events } }) => {
-    console.log('Formatted events: ', eventsFormat(events)[0]);
-    console.log('Locations: ', getLocations(events));
+  .then(({ data }) => {
+    const { events, pagination } = data;
+    const pages = Promise.all(paginationRequest(pagination));
+
+    pages.then(values => console.log(values[0].data.events));
+    // console.log('Formatted events: ', eventsFormat(events)[0]);
+    // console.log('Locations: ', getLocations(events));
   }).catch(err => console.log('ERROR!!! ', err));
 
 
